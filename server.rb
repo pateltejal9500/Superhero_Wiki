ceabd6a811da71ac856754fec6a4e76760d1ca31
pateltejal9500 <pateltejal9500@gmail.com>
2014-09-09 13:03:24 -0400

    heroku

--- server.rb
+++ server.rb
@@ -3,12 +3,7 @@ require 'bundler/setup'
 
 Bundler.require(:default)
 
-group :development do
-  gem 'sinatra-contrib', require: 'sinatra/reloader' 
-  gem 'pry'
-end
-
-require_relative './lib/config/enviroments.rb'
+require_relative './config/enviroments.rb'
 require_relative './lib/changed.rb'
 require_relative './lib/author.rb'
 require_relative './lib/document.rb'
