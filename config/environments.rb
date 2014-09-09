d478119d728271ed39affc2850135c063329e3d4
pateltejal9500 <pateltejal9500@gmail.com>
2014-09-09 12:51:56 -0400

    heroku

--- config/environments.rb
+++ config/environments.rb
@@ -1,14 +1,14 @@
 configure :production, :development do
   db = URI.parse(ENV['DATABSE_URL'] || 'postgres://localhost/mydb')
 
-  ActiveRecord::Base.establish_connection(
+  ActiveRecord::Base.establish_connection({
     :adapter => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
     :host => db.host,
     :username => db.user,
     :password => db.password,
     :database => db.path[1..-1],
     :encoding => 'utf8'
-    )
+    })
 end
 
 
@@ -24,4 +24,4 @@ end
 # :database => "superhero_wiki"
 
 # })
-# ActiveRecord::Base.logger = Logger.new(STDOUT)
+ActiveRecord::Base.logger = Logger.new(STDOUT)
